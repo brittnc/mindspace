40c8e02df886b8800f027622a34688bd4b98b4bb
Hardik Rola <hrsautomation20@gmail.com>
2022-01-13 11:58:10 -0500

    Sign-in/out and Registration

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Auth from '../../utils/auth';
+
+const Header = () => {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+  return (
+    <header className="align-center">
+      <div className="container">
+        <Link className="text-dark" to="/">
+          <h1 className="m-0" style={{ fontSize: '2rem' }}>
+            MindSpace
+          </h1>
+        </Link>
+       
+        <div>
+          {Auth.loggedIn() ? (
+            <>
+              <Link className="btn btn-lg btn-primary m-2" to="/me">
+                View My Profile
+              </Link>
+              <button className="btn btn-lg btn-light m-2" onClick={logout}>
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link className="btn btn-lg btn-primary m-2" to="/login">
+                Login
+              </Link>
+              <Link className="btn btn-lg btn-light m-2" to="/signup">
+                Signup
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
