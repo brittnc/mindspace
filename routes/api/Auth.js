6025266ef7252893b483f1d7871c4a77ed989b33
Hardik Rola <hrsautomation20@gmail.com>
2022-01-20 22:33:51 -0500

    created appointments, auth, log and symptoms routes

---
+++ routes/api/Auth.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+
+module.exports = function(passport, User){
+  router.get('/', (req, res, next) => {
+      res.json('req.user is: ' + req.user);
+    });
+    
+    router.post(
+      '/login',
+      passport.authenticate('local'),
+      (req, res) => {
+        console.log(req.user)
+        res.json({message: "The user is logged in", userId: req.user._id})
+      } 
+    );
+    
+    router.post('/logout', (req, res) => {
+      req.logout();
+      res.json("signed out");
+    });
+    
+
+    router.post('/signup', (req, res, next) => {
+      var user = new User({
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email
+      });
+      console.log("user is " + user);
+      user
+        .save()
+        .then(data => {
+          
+          res.json("everything is good in signup");
+        })
+        .catch(err => res.json(err));
+      // res.redirect('/');
+
+
+    });
+
+  return router;
+}
